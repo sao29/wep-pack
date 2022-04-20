8d4d1187a5144bd0dc95e8d21b8965496d10e090
Salvador Alvarez <sao.95@hotmail.com>
2022-04-19 21:35:35 -0700

    se trabajo con el webpack bundle analyzer

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -5,6 +5,7 @@ const CopyPlugin = require('copy-webpack-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
   entry: './src/index.js',
@@ -81,6 +82,19 @@ module.exports = {
       ]
     }),
     new Dotenv(),
+    new BundleAnalyzerPlugin(),
   ],
- 
+  devServer: {
+    static: 
+    {
+      directory: path.join(__dirname, "dist"),
+      watch: true,
+    },
+    watchFiles: path.join(__dirname, "./**"), //observa los cambios en todos nuestros archivos y actualiza el navegador
+    compress: true,
+    historyApiFallback: true,
+    port: 3006,
+    open: true, //Hace que se abra en el navegador
+    
+  },
 }
